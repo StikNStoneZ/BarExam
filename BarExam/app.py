f0ed4256996d327ece3e1e11b2c62a360b62a523
StikNStoneZ <swastik.composer777@gmail.com>
2026-03-15 14:49:29 +0530

    Update app.py

--- BarExam/app.py
+++ BarExam/app.py
@@ -1,22 +1,24 @@
 from flask import Flask, render_template, request
-from analyzers.bpm_analyzer import analyze_beat
-from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 import os
 
+from analyzers.bpm_analyzer import analyze_beat
 from analyzers.lyric_analyzer import analyze_lyrics
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = "uploads"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
+
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
-
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
@@ -26,15 +28,14 @@ def analyze():
 
     beat_results = None
     lyrics_results = None
-    beat_path = None
 
-    # save beat if uploaded
     if beat and beat.filename != "":
-        beat_path = os.path.join(app.config["UPLOAD_FOLDER"], beat.filename)
+        filename = secure_filename(beat.filename)
+        beat_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+
         beat.save(beat_path)
         beat_results = analyze_beat(beat_path)
 
-    # analyze lyrics if text exists
     if lyrics.strip() != "":
         lyrics_results = analyze_lyrics(lyrics)
 
@@ -43,5 +44,7 @@ def analyze():
         result=lyrics_results,
         beat=beat_results
     )
+
+
 if __name__ == "__main__":
     app.run(debug=True)
